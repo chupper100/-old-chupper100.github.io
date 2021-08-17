d0f1d889efb0eff1269fdd68e5da2c5ad812bfb0
chupper100 <phamminhdat223@gmail.com>
2021-08-17 08:49:05 +0700

    test

---
+++ script.js
@@ -0,0 +1,11 @@
+$(function () {
+    $('*')
+        .data('hovering', false)
+        .mouseover(function () { $(this).data('hovering', true) })
+        .mouseout(function () { $(this).data('hovering', false) });
+});
+
+window.setInterval(function () {
+    $('#div').html("Textbox currently hovered: " +
+        $('input[type="text"]').data('hovering'));
+}, 100)
