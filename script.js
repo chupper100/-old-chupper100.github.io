4910c6b5c9d7e2a4c852899947da2515adbc5b57
chupper100 <phamminhdat223@gmail.com>
2021-12-22 18:26:07 +0700

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
-// $(function () {
-//     $('*')
-//         .data('hovering', false)
-//         .mouseover(function () { $(this).data('hovering', true) })
-//         .mouseout(function () { $(this).data('hovering', false) });
-// });
+const input_box = document.querySelector('input');
+//center input_box
+input_box.style.left = (window.innerWidth - input_box.offsetWidth) / 2 + 'px';
+//input_box value on change 
+input_box.addEventListener("input", function (e) {
+    var value = e.target.value;
+    //check if value is empty
+    if (value == "") {
+        //change background color
+        document.body.style.backgroundColor = "white";
+    } else {
+        //check if value is a rgb color
+        if (value.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/)) {
+            document.body.style.backgroundColor = value;
+            console.log('rgb');
+        } //check if value is a hex color
+        else if (value.match(/^#([0-9a-f]{3}|[0-9a-f]{6})$/i)) {
+            //change background color
+            document.body.style.backgroundColor = value;
+            console.log('hex');
+        } else if (value.match(/^[a-zA-Z]+$/)) {
+            console.log('text');
+            document.body.style.backgroundColor = value;
 
-// window.setInterval(function () {
-//     $('#div-data').html("Textbox currently hovered: " +
-//         $('a').data('hovering'));
-// }, 100)
+        } //rgba
+        else if (value.match(/^rgba\((\d+),\s*(\d+),\s*(\d+),\s*(\d+)\)$/)) {
+            console.log('rgba');
+            document.body.style.backgroundColor = value;
+        }
+
+    }
+});
